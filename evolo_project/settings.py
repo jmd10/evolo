b6b1a907d02e1beaceaf85f7497635c1a8b419f0
jmd_10 <jmd_10@yahoo.com>
2021-11-09 20:34:36 -0500

    initial heroku push v2

--- evolo_project/settings.py
+++ evolo_project/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False 
 
 ALLOWED_HOSTS = ['localhost', 'evolo.herokuapp.com']
 
